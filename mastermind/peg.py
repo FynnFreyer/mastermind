0cc48f6f8e181472d364ee03a4c7ee2b59837a02
Fynn Freyer <fynn.freyer@googlemail.com>
2025-10-19 17:38:46 +0200

    refactor(peg)!: encode pegs as integers

--- mastermind/peg.py
+++ mastermind/peg.py
@@ -3,35 +3,35 @@ This module contains the Peg classes for Mastermind.
 The ``CodePeg`` and ``KeyPeg`` classes represent pegs used for guessing and answering respectively.
 """
 
-from enum import StrEnum
+from enum import IntEnum
 from random import choice
 from typing import Sequence, TypeAlias
 
 
-class CodePeg(StrEnum):
-    RED = "red"
-    GREEN = "green"
-    YELLOW = "yellow"
-    BLUE = "blue"
-    WHITE = "white"
+class CodePeg(IntEnum):
     """Code pegs are used to represent the code and guesses."""
 
+    RED = 0
+    GREEN = 1
+    YELLOW = 2
+    BLUE = 3
+    WHITE = 4
 
     @classmethod
     def random(cls) -> "CodePeg":
         return choice(list(cls.__members__.values()))
 
     def __str__(self) -> str:
-        return self.value[0].upper()
+        return self.name[0]
 
 
-class KeyPeg(StrEnum):
-    RED = "red"
-    WHITE = "white"
+class KeyPeg(IntEnum):
     """Key pegs are used to represent feedback for a guess."""
 
+    RED = 0
     """Correct color and position."""
 
+    WHITE = 1
     """Correct color, wrong position."""
 
 
